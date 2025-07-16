d88e645e7bde8229440479d56b05d6b136d986bf
DaneCooper205 <dane.cooper@use.startmail.com>
2025-07-16 08:37:27 -0500

    Starter commit

---
+++ src/main/java/com/cooper/aliens/solarsystem/SolarsystemApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.aliens.solarsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarsystemApplication.class, args);
+	}
+
+}
